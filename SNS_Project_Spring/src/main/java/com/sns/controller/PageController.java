571612ed156917a8dba3629d1b99b293567001c6
dla3933 <dla3933@gmail.com>
2017-12-09 20:35:42 +0900

    171209 logging & mybatis setting.

--- SNS_Project_Spring/src/main/java/com/sns/controller/PageController.java
+++ SNS_Project_Spring/src/main/java/com/sns/controller/PageController.java
@@ -1,15 +1,7 @@
 package com.sns.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 import java.util.Map;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,14 +9,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.sns.service.testService;
 
 @Controller
 @RequestMapping("page")
 public class PageController {
+	
+	@Autowired
+	private testService service;
+	
 	@RequestMapping("index.html")
 	public String index(@RequestParam Map map, Model model, HttpSession session) {	
 		return "index";
@@ -32,6 +28,7 @@ public class PageController {
 	
 	@RequestMapping("login.html")
 	public String login(Model model, HttpServletRequest req, HttpServletResponse res) {
+		System.out.println("test :: " + service.test().toString());
 		return "login";
 	}
 }
